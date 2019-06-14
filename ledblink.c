7f3b33fb43f132c5a877394f12b1941ce61679e1
Ardavast Dayleryan <ardavast@noiseoverip.com>
2019-06-14 14:34:45 +0300

    Remove trailing whitespace

--- ledblink.c
+++ ledblink.c
@@ -2,21 +2,21 @@
 
 #define STACK_TOP 0x20002000
 
-void nmi_handler(void);												
+void nmi_handler(void);
 void hardfault_handler(void);
-void delay(void); 
+void delay(void);
 int main(void);
 
-uint32_t *myvectors[4] 
+uint32_t *myvectors[4]
 __attribute__ ((section("vectors")))= {
-    (uint32_t *)STACK_TOP,         
-    (uint32_t *)main,              
-    (uint32_t *)nmi_handler,       
-    (uint32_t *)hardfault_handler  
+    (uint32_t *)STACK_TOP,
+    (uint32_t *)main,
+    (uint32_t *)nmi_handler,
+    (uint32_t *)hardfault_handler
 };
 
 int main(void)
-{	
+{
 	RCC->APB2ENR |= 0x4; /* GPIOA clock enable */
 	GPIOA->CRH = (GPIOA->CRH & ~(0xf0)) | 0x20; /* PA9 output max 2MHz, push-pull */
 
@@ -28,7 +28,7 @@ int main(void)
 	}
 }
 
-void delay(void) 
+void delay(void)
 {
 	int i;
 
